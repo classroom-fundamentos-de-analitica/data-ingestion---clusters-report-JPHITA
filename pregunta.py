1089ef68be79b34af10298ab3527186fbd96d168
JPHITA <jphita18@gmail.com>
2022-09-17 20:07:12 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -14,8 +14,38 @@ import pandas as pd
 
 def ingest_data():
 
-    #
-    # Inserte su código aquí
-    #
+    df = pd.read_fwf(
+        "clusters_report.txt",
+        widths=[7, 16, 16, 78],
+        header=[0],
+        skiprows=[1, 2, 3]
+    )
+
+    lastCluster = 1
+    lastPClave = 105
+    lastPorcentaje = "15,9 %"
+    for i, _ in df.iterrows():
+        if df.iloc[i,0] != lastCluster and not pd.isna(df.iloc[i,0]):
+            lastCluster = df.iloc[i,0]
+            lastPClave = df.iloc[i,1]
+            lastPorcentaje = df.iloc[i,2]
+        else:
+            df.iloc[i,0] = lastCluster
+            df.iloc[i,1] = lastPClave
+            df.iloc[i,2] = lastPorcentaje
+
+    df = df.groupby(["Cluster", "Cantidad de", "Porcentaje de"])
+
+    df = df.agg(lambda x: " ".join(x)).reset_index()
+
+    # quitar espacios repetidos entre palabras
+    df["Principales palabras clave"] = df["Principales palabras clave"].str.replace(r"\s{2,}", " ", regex=True)
+
+    # quitar los puntos
+    df["Principales palbras clave"] = df["Principales palabras clave"].str.replace(".", "")
+
+    # arreglar los porcentajes
+    df["Porcentaje de"] = df["Porcentaje de"].str.slice(0, -2)
+    df["Porcentaje de"] = df["Porcentaje de"].str.replace(",", ".").astype(float)
 
     return df
